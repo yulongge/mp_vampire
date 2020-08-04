5fe3c2c6dc62878f60d36cca44e7593d02c461ea
edz <gyl@acewill.cn>
2020-08-05 00:01:27 +0800

    引入less less-laoder 把less文件单独提出来

---
+++ README.md
@@ -0,0 +1,8 @@
+# Vampire 小程序
+
+## 架构优化
+### 支持less
+
+```sh
+npm i less less-loader -D
+```
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -25,29 +25,6 @@
 	}
 </script>
 
-<style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
-
-	.avatar {
-		height: 200rpx;
-		width: 200rpx;
-		margin: 200rpx auto 50rpx auto;
-		border-radius: 50%;
-		overflow: hidden;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+<style lang="less">
+	@import url(./style.less);
 </style>
