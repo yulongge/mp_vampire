e96bbe7d9e56076e2834bdc403aaae8851bb68cf
gaiyulong <gyl@acewill.cn>
2020-08-08 18:53:39 +0800

    dev

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,14 @@
 	export default {
 		onLaunch: function() {
 			console.log('App Launch')
+			if(!wx.cloud) {
+				console.error('请使用2.2.3或以上的基础库版本，进行云开发')
+			} else {
+				wx.cloud.init({
+					traceUser: true,
+					env: "vampire-gyl"
+				})
+			}
 		},
 		onShow: function() {
 			console.log('App Show')
